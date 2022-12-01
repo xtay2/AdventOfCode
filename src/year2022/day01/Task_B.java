68ec4a043e0c79261d4a586e6d2eec85678403cc
xtay2 <x-tay2@o2mail.de>
2022-12-01 07:54:31 +0100

    fix(Fontspacing)

--- src/year2022/day01/Task_B.java
+++ src/year2022/day01/Task_B.java
@@ -17,18 +17,18 @@ public class Task_B extends Task {
 	@Override
 	protected Object exec(AdventOfCode aoc) {
 		return new Generator<>(aoc.inputTxt())
-				.map(f -> new Generator<>(f.split("\n\n"))    // Teile String in Gruppen
-						.map(v -> new Generator<>(v.split("\n"))    // Teile Gruppen in Einträge
-								.map(Integer::parseInt)    // Konvertiere Einträge zu Zahlen
-								.reduce(0, Integer::sum))    // Berechne die Summe der Einträge jeder Gruppe
+				.map(f -> new Generator<>(f.split("\n\n"))       // Teile String in Gruppen
+						.map(v -> new Generator<>(v.split("\n")) // Teile Gruppen in Einträge
+								.map(Integer::parseInt)          // Konvertiere Einträge zu Zahlen
+								.reduce(0, Integer::sum))        // Berechne die Summe der Einträge jeder Gruppe
 				)
 				.flatten()
 				.stream()
 				.map(i -> ((Integer) i))
-				.sorted(Comparator.reverseOrder())    // Sortiere absteigend
+				.sorted(Comparator.reverseOrder())               // Sortiere absteigend
 				.mapToInt(i -> i)
-				.limit(3)    // Nimm die ersten 3
-				.sum();    // Summiere
+				.limit(3)                                        // Nimm die ersten 3
+				.sum();                                          // Summiere
 	}
 }
 
