fe2419cec3fa8bbdc1307db078c552079b3e8729
xtay2 <x-tay2@o2mail.de>
2022-12-01 07:40:20 +0100

    fix(Commented)

--- src/year2022/day01/Task_A.java
+++ src/year2022/day01/Task_A.java
@@ -13,16 +13,16 @@ public class Task_A extends Task {
 	@Override
 	protected Object exec(AdventOfCode aoc) {
 		return new Generator<>(aoc.inputTxt())
-				.map(f -> new Generator<>(f.split("\n\n"))
-						.map(v -> new Generator<>(v.split("\n"))
-								.map(Integer::parseInt)
-								.reduce(0, Integer::sum))
+				.map(f -> new Generator<>(f.split("\n\n"))            // Teile String in Gruppen
+						.map(v -> new Generator<>(v.split("\n"))      // Teile Gruppen in Einträge
+								.map(Integer::parseInt)                     // Konvertiere Einträge zu Zahlen
+								.reduce(0, Integer::sum))            // Berechne die Summe der Einträge jeder Gruppe
 				)
 				.flatten()
 				.stream()
 				.mapToInt(i -> ((Integer) i))
-				.max()
-				.getAsInt();
+				.max()                                                      // Berechne Maximum aller Summen
+				.getAsInt();                                                // Konvertiere Optional zu int
 	}
 }
 
