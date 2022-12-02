f213979a367fbe3872664d96b30b5ecee466083c
xtay2 <x-tay2@o2mail.de>
2022-12-02 08:24:37 +0100

    done(Task B)

--- src/year2022/day02/Task_B.java
+++ src/year2022/day02/Task_B.java
@@ -4,13 +4,12 @@ import aoc.AdventOfCode;
 import aoc.Task;
 import helper.util.Generator;
 
-import java.util.Objects;
+import java.util.Arrays;
 
-
-public class Task_A extends Task {
+public class Task_B extends Task {
 
 	static {
-		new Task_A();
+		new Task_B();
 	}
 
 	@Override
@@ -18,13 +17,10 @@ public class Task_A extends Task {
 		return new Generator<>(aoc.inputLst())
 				.map(l -> l.split(" "))
 				.map(a -> new Integer[]{a[0].charAt(0) - 'A' + 1, a[1].charAt(0) - 'X' + 1}) // Konvertiere zu zahlen
-				.map(a -> (a[0] == a[1] // DRAW
-						? 3
-						: (a[0] == (a[1] + 1) % 3 + 1) // WIN
-						? 6
-						: 0)
-						+ a[1] // HAND
-				)
+				.map(a -> {
+					int x = a[0], y = (x + a[1]) % 3, res = y + 1;
+					return (x - 1 == y ? 3 : x % 3 == y ? 6 : 0) + res;
+				})
 				.reduce(0, Integer::sum);
 	}
 }
