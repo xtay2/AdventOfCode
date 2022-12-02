25f056c37dc76d7326f8d40b5022e06102ca33c6
xtay2 <x-tay2@o2mail.de>
2022-12-02 08:46:59 +0100

    pretty(Task B)

--- src/year2022/day02/Task_B.java
+++ src/year2022/day02/Task_B.java
@@ -18,8 +18,14 @@ public class Task_B extends Task {
 				.map(l -> l.split(" "))
 				.map(a -> new Integer[]{a[0].charAt(0) - 'A' + 1, a[1].charAt(0) - 'X' + 1}) // Konvertiere zu zahlen
 				.map(a -> {
-					int x = a[0], y = (x + a[1]) % 3, res = y + 1;
-					return (x - 1 == y ? 3 : x % 3 == y ? 6 : 0) + res;
+					a[1] = (a[0] + a[1]) % 3;
+					int hand = a[1] + 1; // +1 wegen Modulo
+					return (a[0] - 1 == a[1] // DRAW
+							? 3
+							: a[0] % 3 == a[1]  // WIN
+							? 6
+							: 0)
+							+ hand;  // HAND (1, 2, 3)
 				})
 				.reduce(0, Integer::sum);
 	}
