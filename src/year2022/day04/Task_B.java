7f7224ca024689731f8070f7015c7368ba192b2d
xtay2 <x-tay2@o2mail.de>
2022-12-04 06:32:42 +0100

    pretty(Day 4)

--- src/year2022/day04/Task_B.java
+++ src/year2022/day04/Task_B.java
@@ -2,6 +2,7 @@ package year2022.day04;
 
 import aoc.AdventOfCode;
 import aoc.Task;
+import helper.util.Generator;
 
 public class Task_B extends Task {
 
@@ -11,6 +12,23 @@ public class Task_B extends Task {
 
 	@Override
 	protected Object exec(AdventOfCode aoc) {
-		return null;
+		return new Generator<>(aoc.inputLst())
+				.map(line -> new Generator<>(line.split(","))
+						.map(group -> new Generator<>(group.split("-"))
+								.map(Integer::parseInt)
+						).flatten()
+						.map(v -> (Integer) v)
+						.list()
+				)
+				.filter(v -> contains(v.get(0), v.get(1), v.get(2), v.get(3)))
+				.length();
+	}
+
+
+	boolean contains(int lower1, int upper1, int lower2, int upper2) {
+		return (lower2 <= upper1 && upper1 <= upper2)
+				|| (lower2 <= lower1 && lower1 <= upper2)
+				|| (lower1 <= upper2 && upper2 <= upper1)
+				|| (lower1 <= lower2 && lower2 <= upper1);
 	}
 }
---
+++ src/year2022/day05/Task_A.java
@@ -0,0 +1,16 @@
+package year2022.day05;
+
+import aoc.AdventOfCode;
+import aoc.Task;
+
+public class Task_A extends Task {
+
+	static {
+		new Task_A();
+	}
+
+	@Override
+	protected Object exec(AdventOfCode aoc) {
+		return null;
+	}
+}
