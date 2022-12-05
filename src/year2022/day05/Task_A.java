a6b53e404e2df2a8a6e3301b1fc96510bc45ffc0
xtay2 <x-tay2@o2mail.de>
2022-12-05 06:56:38 +0100

    pretty(Day 5)

--- src/year2022/day05/Task_A.java
+++ src/year2022/day05/Task_A.java
@@ -3,6 +3,13 @@ package year2022.day05;
 import aoc.AdventOfCode;
 import aoc.Task;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.Integer.*;
+
 public class Task_A extends Task {
 
 	static {
@@ -11,6 +18,42 @@ public class Task_A extends Task {
 
 	@Override
 	protected Object exec(AdventOfCode aoc) {
-		return null;
+		var lines = aoc.inputLst();
+		List<Character>[] stacks = buildStacks(lines); // Parse Crates
+
+		for (var line : lines) {
+			if (!line.isBlank()) {
+				move(
+						stacks,
+						parseInt(line.substring(line.indexOf("move ") + 5, line.indexOf(" from"))),
+						parseInt(line.substring(line.indexOf("from ") + 5, line.indexOf(" to"))),
+						parseInt(line.substring(line.indexOf("to ") + 3))
+				);
+			}
+		}
+
+		return Arrays.stream(stacks).map(e -> e.get(0).toString()).collect(Collectors.joining(""));
+	}
+
+	List<Character>[] buildStacks(List<String> lines) {
+		List<Character>[] stacks = new ArrayList[9];
+		Arrays.setAll(stacks, i -> new ArrayList<>());
+		while (lines.get(0).startsWith("[")) {
+			var line = lines.remove(0);
+			for (int i = 0; i < stacks.length; i++) {
+				var c = line.charAt((i * 4) + 1);
+				if (c != ' ')
+					stacks[i].add(c);
+			}
+		}
+		lines.remove(0); // Delete numbered line
+		return stacks;
+	}
+
+	void move(List<Character>[] stacks, int amount, int from, int to) {
+		for (int moves = 0; moves < amount; moves++) {
+			var crate = stacks[from - 1].remove(0);
+			stacks[to - 1].add(0, crate);
+		}
 	}
 }
---
+++ src/year2022/day06/Task_B.java
@@ -0,0 +1,16 @@
+package year2022.day06;
+
+import aoc.AdventOfCode;
+import aoc.Task;
+
+public class Task_B extends Task {
+
+	static {
+		new Task_B();
+	}
+
+	@Override
+	protected Object exec(AdventOfCode aoc) {
+		return null;
+	}
+}
