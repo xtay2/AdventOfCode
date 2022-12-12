ecf4e930afc9c3868944d0f2c919219ed976a8c8
xtay2 <x-tay2@o2mail.de>
2022-12-12 02:05:38 +0100

    pretty(Year 2022, Day 11, Task A & B)

--- src/year2022/day11/Task_A.java
+++ src/year2022/day11/Task_A.java
@@ -3,6 +3,15 @@ package year2022.day11;
 import aoc.AdventOfCode;
 import aoc.Task;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+import static year2022.day11.Task_A.Monkey.monkeys;
+
+
 public class Task_A extends Task {
 
 	static {
@@ -11,15 +20,62 @@ public class Task_A extends Task {
 
 	@Override
 	protected Object exec(AdventOfCode aoc) {
-		var lines = aoc.inputLst();
-		int exec = 0, wait = 0;
-		int cnt = 0;
-		for (int i = 0; i < lines.size(); i++) {
-			if (((i + 1) - 20) % 40 == 0)
-				cnt += exec * (i + 1);
-			exec = wait;
-			wait = lines.get(i).startsWith("noop") ? 0 : Integer.parseInt(lines.get(i).split(" ")[1]);
+		var input = aoc.inputLst();
+		while (!input.isEmpty()) {
+			if (input.get(0).isBlank() || input.get(0).startsWith("Monkey")) {
+				input.remove(0);
+				continue;
+			}
+			monkeys.add(new Monkey(
+					Arrays.stream(input.remove(0).split(": ")[1].split(", ")).mapToInt(Integer::parseInt).toArray(),
+					buildExp(input.remove(0).split("new = ")[1]),
+					Integer.parseInt(input.remove(0).split("divisible by ")[1]),
+					Integer.parseInt(input.remove(0).split("monkey ")[1]),
+					Integer.parseInt(input.remove(0).split("monkey ")[1])
+			));
 		}
-		return cnt;
+		for (int round = 0; round < 20; round++)
+			monkeys.forEach(Monkey::throwItems);
+		return monkeys.stream().map(m -> m.business)
+				.sorted(Comparator.reverseOrder())
+				.limit(2).reduce((a, b) -> a * b).orElseThrow();
+	}
+
+	static Function<Integer, Integer> buildExp(String exp) {
+		if (exp.equals("old * old"))
+			return x -> x * x;
+		int y = Integer.parseInt(exp.split(" . ")[1]);
+		if (exp.matches("old \\+ \\d+"))
+			return x -> x + y;
+		if (exp.matches("old \\* \\d+"))
+			return x -> x * y;
+		throw new AssertionError("Unknown Expression: " + exp);
+	}
+
+	static class Monkey {
+
+		static List<Monkey> monkeys = new ArrayList<>();
+
+		int business = 0;
+
+		final List<Integer> items = new ArrayList<>();
+		final Function<Integer, Integer> operation;
+		final int divBy, targetTrue, targetFalse;
+
+		public Monkey(int[] items, Function<Integer, Integer> operation, int divBy, int targetTrue, int targetFalse) {
+			this.items.addAll(Arrays.stream(items).boxed().toList());
+			this.operation = operation;
+			this.divBy = divBy;
+			this.targetTrue = targetTrue;
+			this.targetFalse = targetFalse;
+		}
+
+		void throwItems() {
+			while (!items.isEmpty()) {
+				business++;
+				int worry = operation.apply(items.remove(0)) / 3;
+				monkeys.get(worry % divBy == 0 ? targetTrue : targetFalse).items.add(worry);
+			}
+		}
 	}
 }
--- src/year2022/day11/Task_B.java
+++ src/year2022/day11/Task_B.java
@@ -3,14 +3,87 @@ package year2022.day11;
 import aoc.AdventOfCode;
 import aoc.Task;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+import static year2022.day11.Task_B.Monkey.monkeys;
+
+
 public class Task_B extends Task {
 
 	static {
 		new Task_B();
 	}
 
+	static int lcd = 1;
+
 	@Override
 	protected Object exec(AdventOfCode aoc) {
-		return null;
+		var input = aoc.inputLst();
+		while (!input.isEmpty()) {
+			if (input.get(0).isBlank() || input.get(0).startsWith("Monkey")) {
+				input.remove(0);
+				continue;
+			}
+			monkeys.add(new Monkey(
+					Arrays.stream(input.remove(0).split(": ")[1].split(", ")).mapToLong(Long::parseLong).toArray(),
+					buildExp(input.remove(0).split("new = ")[1]),
+					Integer.parseInt(input.remove(0).split("divisible by ")[1]),
+					Integer.parseInt(input.remove(0).split("monkey ")[1]),
+					Integer.parseInt(input.remove(0).split("monkey ")[1])
+			));
+		}
+		lcd = monkeys.stream().map(m -> m.divBy).reduce((a, b) -> { // LCD-Algorithm
+			int higher = Math.max(a, b), lower = Math.min(a, b), d = higher;
+			while (d % lower != 0)
+				d += higher;
+			return d;
+		}).orElseThrow();
+		for (int round = 0; round < 10000; round++)
+			monkeys.forEach(Monkey::throwItems);
+		return monkeys.stream().map(m -> m.business)
+				.sorted(Comparator.reverseOrder())
+				.limit(2).reduce((a, b) -> a * b).orElseThrow();
+	}
+
+	static Function<Long, Long> buildExp(String exp) {
+		if (exp.equals("old * old"))
+			return x -> x * x;
+		int y = Integer.parseInt(exp.split(" . ")[1]);
+		if (exp.matches("old \\+ \\d+"))
+			return x -> x + y;
+		if (exp.matches("old \\* \\d+"))
+			return x -> x * y;
+		throw new AssertionError("Unknown Expression: " + exp);
+	}
+
+	static class Monkey {
+
+		static List<Monkey> monkeys = new ArrayList<>();
+
+		long business = 0;
+
+		final List<Long> items = new ArrayList<>();
+		final Function<Long, Long> operation;
+		final int divBy, targetTrue, targetFalse;
+
+		public Monkey(long[] items, Function<Long, Long> operation, int divBy, int targetTrue, int targetFalse) {
+			this.items.addAll(Arrays.stream(items).boxed().toList());
+			this.operation = operation;
+			this.divBy = divBy;
+			this.targetTrue = targetTrue;
+			this.targetFalse = targetFalse;
+		}
+
+		void throwItems() {
+			while (!items.isEmpty()) {
+				business++;
+				long worry = operation.apply(items.remove(0)) % lcd;
+				monkeys.get(worry % divBy == 0 ? targetTrue : targetFalse).items.add(worry);
+			}
+		}
 	}
 }
