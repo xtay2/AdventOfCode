733b530ad2813e85378e40fdef01c92979f2d767
xtay2 <x-tay2@o2mail.de>
2023-12-06 19:51:42 +0100

    feat(01/22)

--- src/year2022/day01/Task_A.java
+++ src/year2022/day01/Task_A.java
@@ -2,27 +2,24 @@ package year2022.day01;
 
 import aoc.AdventOfCode;
 import aoc.Task;
-import helper.util.Generator;
+
+import java.util.Arrays;
+
+import static java.util.Arrays.stream;
 
 public class Task_A extends Task {
 
-	static {
-		new Task_A();
-	}
+    static {
+        new Task_A();
+    }
 
-	@Override
-	protected Object exec(AdventOfCode aoc) {
-		return new Generator<>(aoc.inputTxt())
-				.map(f -> new Generator<>(f.split("\n\n"))                 // Teile String in Gruppen
-						.map(v -> new Generator<>(v.split("\n"))   // Teile Gruppen in Einträge
-								.map(Integer::parseInt)    // Konvertiere Einträge zu Zahlen
-								.reduce(0, Integer::sum))  // Berechne die Summe der Einträge jeder Gruppe
-				)
-				.flatten()
-				.stream()
-				.mapToInt(i -> ((Integer) i))
-				.max()                                           // Berechne Maximum aller Summen
-				.getAsInt();                                     // Konvertiere Optional zu int
-	}
+    @Override
+    protected Object exec(AdventOfCode aoc) {
+        return stream(aoc.inputTxt().split("\n\n"))
+                .mapToInt(group -> Arrays.stream(group.split("\n"))
+                        .mapToInt(Integer::parseInt)
+                        .sum())
+                .max()
+                .orElseThrow();
+    }
 }
-
--- src/year2022/day01/Task_B.java
+++ src/year2022/day01/Task_B.java
@@ -2,11 +2,10 @@ package year2022.day01;
 
 import aoc.AdventOfCode;
 import aoc.Task;
-import helper.util.Generator;
+import helper.util.ArrayHelper;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import static helper.util.StreamHelper.reverse;
+import static java.util.Arrays.stream;
 
 public class Task_B extends Task {
 
@@ -16,19 +15,10 @@ public class Task_B extends Task {
 
 	@Override
 	protected Object exec(AdventOfCode aoc) {
-		return new Generator<>(aoc.inputTxt())
-				.map(f -> new Generator<>(f.split("\n\n"))                       // Teile String in Gruppen
-						.map(v -> new Generator<>(v.split("\n"))         // Teile Gruppen in Einträge
-								.map(Integer::parseInt)          // Konvertiere Einträge zu Zahlen
-								.reduce(0, Integer::sum))        // Berechne die Summe der Einträge jeder Gruppe
-				)
-				.flatten()
-				.stream()
-				.map(i -> ((Integer) i))
-				.sorted(Comparator.reverseOrder())               // Sortiere absteigend
-				.mapToInt(i -> i)
-				.limit(3)                                        // Nimm die ersten 3
-				.sum();                                          // Summiere
+		return reverse(stream(aoc.inputTxt().split("\n\n"))
+				.mapToInt(group -> ArrayHelper.parseIntStream(group.split("\n")).sum())
+				.sorted()
+		).limit(3).sum();
 	}
 }
 
