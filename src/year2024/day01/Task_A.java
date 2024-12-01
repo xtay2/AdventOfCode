8468aea534561a6ea1b9850d66d84913a778d32b
TriNextDennis <dennis_woithe@web.de>
2024-12-01 22:38:13 +0100

    fix(minor)

--- src/year2024/day01/Task_A.java
+++ src/year2024/day01/Task_A.java
@@ -3,8 +3,6 @@ package year2024.day01;
 import aoc.AdventOfCode;
 import aoc.Task;
 
-import java.util.Arrays;
-
 public class Task_A extends Task {
 
     static {
@@ -14,20 +12,14 @@ public class Task_A extends Task {
     @Override
     protected Object exec(AdventOfCode aoc) {
         var tupleList = aoc.inputStr()
-                .map(line -> line.split("\s+"))
+                .map(line -> line.split(" +"))
                 .map(tup -> new int[]{Integer.parseInt(tup[0]), Integer.parseInt(tup[1])})
                 .toList();
-        var leftList = tupleList.stream().mapToInt(tup -> tup[0]).toArray();
-        var rightList = tupleList.stream().mapToInt(tup -> tup[1]).toArray();
-        Arrays.sort(leftList);
-        Arrays.sort(rightList);
+        var leftList = tupleList.stream().mapToInt(tup -> tup[0]).sorted().toArray();
+        var rightList = tupleList.stream().mapToInt(tup -> tup[1]).sorted().toArray();
         long sum = 0;
         for (int i = 0; i < leftList.length && i < rightList.length; i++)
-            sum += distance(leftList[i], rightList[i]);
+            sum += Math.abs(leftList[i] - rightList[i]);
         return sum;
-    }
-
-    private long distance(int a, int b) {
-        return Math.abs(a - b);
     }
 }
